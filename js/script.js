168d6128faa6fa9f4674eefbbe9fca91d401559e
Teddy <ochiengnyateya@gmail.com>
2022-03-14 09:38:37 +0300

    This commit adds more js functions to the page.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 function showMessage(){
     alert("Hello!,Your Message has been received");
 }
+
+$('.switch').hover(function() {
+    $(this).closest('.showWords').hide();
+    $(this).next('div').closest('.portfolioParagraph').show();
+}, function() {
+    $(this).next('div').closest('.portfolioParagraph').hide();
+    $(this).closest('.showWords').show();
+});
+
+(function($) {window.fnames = new Array(); window.ftypes = new Array();
+    fnames[0]='EMAIL';
+    ftypes[0]='email';
+    fnames[1]='FNAME';
+    ftypes[1]='text';
+    fnames[2]='LNAME';
+    ftypes[2]='text';
+    fnames[3]='MMERGE3';
+    ftypes[3]='text';}
+    
+    (jQuery));var $mcj = jQuery.noConflict(true);
